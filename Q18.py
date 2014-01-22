4f65dabc65251565f277c385ad38ce75cd9652df
Thiago Moreira de souza Arrais <thiagomds.scientist@gmail.com>
2014-01-22 21:57:32 -0200

    Exercise python 18 - primeira parte

---
+++ Q18.py
@@ -0,0 +1,13 @@
+#! usr/bin/env python3
+
+##########################################################
+#Faça um Programa que peça uma data no formato dd/mm/aaaa
+#e determine se a mesma é uma data válida.
+##########################################################
+
+def validarData():
+
+    #obter data dd/mm/aaaa
+    obterData = input("Informe uma data:")
+
+    #tratar data obtida.
