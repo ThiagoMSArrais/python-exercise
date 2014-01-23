7291d7c9401db864a7c3f1fbe08cf72fdbe9aa92
Thiago Moreira de souza Arrais <thiagomds.scientist@gmail.com>
2014-01-22 22:40:00 -0200

    Exercicio python 18 - parte final.

--- Q18.py
+++ Q18.py
@@ -5,9 +5,39 @@
 #e determine se a mesma é uma data válida.
 ##########################################################
 
+#TODO REFAZER O CÓDIGO MINIMIZANDO A CONDIÇÃO.
+
 def validarData():
 
     #obter data dd/mm/aaaa
     obterData = input("Informe uma data:")
 
-    #tratar data obtida.
+    #verificar formatação.
+    if len(obterData) == 10:
+        if obterData[2] == '/' and obterData[5] == '/':    
+            #tratar data obtida.
+            dataFatiada = obterData.split('/')
+            #verificar dia.
+            if int(dataFatiada[0]) <= 31 and int(dataFatiada[0]) >= 1:
+                #verificar mês.
+                if int(dataFatiada[1]) <= 12 and int(dataFatiada[1]) >= 1:
+                    #verificar ano.
+                    if len(dataFatiada[2]) == 4 and int(dataFatiada[2]) != 0000:
+                        print ("Data válida!")
+                    else:
+                        print ("Formato ano errado.")
+                        validarData()
+                else:
+                    print ("Formato mês errado, tem que ser entre 1 e 12.")
+                    validarData()
+            else:
+                print ("Formato dia errado, tem que ser entre 1 e 31.")
+                validarData()
+        else:
+            print ("Formato errado da data, o correto seria \"dd/mm/aaaa\".")
+            validarData()
+    else:
+        print ("Quantidade de caracteres errado, formato correto \"dd/mm/aaaa\".")
+        validarData()
+
+validarData()        
