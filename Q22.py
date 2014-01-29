f8c30592cc24909d35d75aac8b2e7430647a42e7
Thiago Moreira de souza Arrais <thiagomds.scientist@gmail.com>
2014-01-29 21:33:18 -0200

    Exercicio python 22

---
+++ Q22.py
@@ -0,0 +1,23 @@
+#! usr/bin/env python3
+
+#Faça um Programa que peça um número inteiro e determine se ele é par ou impar.
+#Dica: utilize o operador módulo (resto da divisão)
+
+
+def verificarNumero():
+
+    try:
+        #Solicita um numero inteiro.
+        numero = int(input("Por favor, informe um número:"))
+
+        #Verificar se o valor de numero eh PAR ou IMPAR.
+        if numero % 2 == 0:
+            print ("Par!")
+        else:
+            print ("Impar!")
+
+    except ValueError:
+        print ("Por favor, informe somente um valor numerico.")
+        verificarNumero()
+
+verificarNumero()
