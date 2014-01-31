0bb7c789e5518f7d1b3aa3b5104ceb5fa7cf284e
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2014-01-31 11:24:18 -0200

    Update Q24.py
    segunda parte

--- Q24.py
+++ Q24.py
@@ -7,3 +7,19 @@
 #     positivo ou negativo;
 #     inteiro ou decimal.
 ########################################################################################################
+
+
+def tratarNumeros():
+
+    #lista de numeros.
+    lista_numeros = []
+    
+    #fazer o for e obter os valores.
+    for i in range(1,3):
+        #obter os valores
+        lista_numeros.append(input("Por favor, informe um número:"))
+        
+        #verificar se é par ou ímpar.
+        if lista_numeros[i] % 2 == 0:
+            
+    
