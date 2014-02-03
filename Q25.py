2b0796ec14997f31a7e081e6daa4a74dc1490c57
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2014-02-03 18:35:49 -0200

    Update Q25.py
    Exercício python 25 - parte final (talvez eu melhore a parte final a respeito do tratamento de dados do status)

--- Q25.py
+++ Q25.py
@@ -12,3 +12,49 @@
 #entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado
 #como "Inocente".
 ############################################################################################
+
+
+def gerarPerguntas():
+  
+    #armazenar quantidade resultado positivo.
+    quantidade_positivo = 0
+
+    #armazenar uma lista de perguntas.
+    lista_perguntas = ["Telefonou para a vítima?",
+                       "Esteve no local do crime?",
+                       "Mora perto da vítima?",
+                       "Devia para a vítima?",
+                       "Já trabalhou com a vítima?"]
+              
+    #fazer as perguntas.
+    for index in len(lista_perguntas):
+        #efetuar a pergunta.
+        print (lista_perguntas[index] + " (sim ou não).")
+        
+        #Obter uma resposta.
+        resposta = input("Resp.:")
+        
+        #Analisar a resposta e se for positivo incrementar o valor
+        if resposta.lower() == "sim":
+            #incrementar mais um.
+            quantidade_positivo += 1
+            
+
+    #verificar status da pessoa.
+    if quantidade_positivo == 0:
+        #imprimir Inocente.
+        print ("Inocente")
+    
+    elif quantidade_positivo == 2:
+        #imprimir Suspeito.
+        print ("Suspeito")
+        
+    elif quantidade_positivo == 4 and quantidade_positivo == 3:
+        #imprimir cúmplice
+        print ("Cúmplice")
+        
+    elif quantidade_positivo == 5:
+        #imprimir Culpado
+        print ("Culpado")
+        
+        
