b473a67b249ad19940e235a9364607a0ee78f4a5
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2014-02-05 11:02:23 -0200

    Exercício Python 26 - começo
    Primeira parte de implementação do código (obter quantidade de litros, obter tipo de combustível)

--- Q26.py
+++ Q26.py
@@ -13,3 +13,22 @@
 # o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor
 # a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
 #####################################################################################################################
+
+
+def descontoCombustivel():
+
+    #dados amarzenados sobre tipo de combustível, preço, desconto até 20 litros e desconto acima de 20 litros.
+    desconto = [("gasolina", 2.50, 4, 6), ("alcool", 1.90, 3, 5)]
+    
+    try:
+        #obter quantidade de litro(s).
+        litro = float(input("Por favor, informe a quantidade de litro que deseja:"))
+    
+    except ValueError:
+        print ("Por favor, informe um valor númerico.")
+        descontoCombustivel()
+    
+    #obter tipo de combustível.
+    tipo_combustivel = input("Por favor, informe o tipo de combustível desejado (Gasolina ou Álcool):")
+    
+    
