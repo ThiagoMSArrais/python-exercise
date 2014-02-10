a4554b58b7166ee231de37400bd9602241872043
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2014-02-10 11:20:06 -0200

    Exercício python 27
    Inserindo dados, capturando dados do produto, peso e tratando dados.

--- Q27.py
+++ Q27.py
@@ -9,3 +9,39 @@
 #receberá ainda um desconto de 10% sobre este total. Escreva um algoritmo para ler a quantidade (em Kg) de morangos
 #e a quantidade (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente.
 ####################################################################################################################
+
+
+def calcular_precos():
+  
+    #variável
+    count = 0
+  
+    #dados do produto.
+    dados_produto = [("morango", 2.50, 2.20), ("maçã", 1.80, 1.50)]
+    
+    while True:
+      
+        #perguntar qual tipo de produto desejado.
+        produto = input("Por favor, informe o produto desejado:")
+        
+        #verificar se tem o produto desejado
+        if produto in dados_produto[count]:
+            break
+        
+        else:
+            #verificar se está acima de dois.
+            if count > 2:
+                print ("Valor inválido.")
+                count = 0
+    
+    while True:
+        
+        #obter o peso do produto.
+        peso = float(input("Por favor, informe o peso desejado:"))
+        
+        #verificar se o peso está acima de zero.
+        if peso > 0:
+            break
+          
+        else:
+            continue
