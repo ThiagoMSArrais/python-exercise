4abea4b3b836e680c5cca3134e6fdeb7aa6614e2
Thiago Moreira de souza Arrais <thiagomds.scientist@gmail.com>
2014-02-11 20:13:12 -0200

    parte final

--- Q28.py
+++ Q28.py
@@ -20,19 +20,70 @@ def fazer_pedido():
 
     #variável
     count = 0
+    total_preco = 0.0
+    tipo_pagamento = ""
+    tipo_carne = ""
+    tipo_pagamento = ""
+    desconto = 0
 
     #dados do produto
     dados_produto = [("file duplo", 4.90, 5.80), ("alcatra", 5.90, 6.80), ("picanha", 6.90, 7.80)]
     
     while True:
-        
+        #varial finalizar do tipo booleano para 
+        finalizar = False
+
         #obter o tipo de carne.
         tipo_carne = input("Por favor, informe o tipo de carne que deseja:")
         
-        #verificar se foi escolhiado a carne que estã na lista corretamente.
-        if tipo_carne.lower() in dados_produto[count]:
-            break
+        for index in dados_produto:
+            #verificar se foi escolhiado a carne que estã na lista corretamente.
+            if tipo_carne.lower() in dados_produto[index][0]:
+                count = index
+                finalizar = True
+                break
         
         else:
+            #Imprimindo na tela "valor invalido!".
             print ("Valor inválido!")
-            count = 0
+        
+        if finalizar:
+            break
+
+        else:
+            continue
+
+    while True:
+
+        try:
+            #obter o valor do peso da carne.
+            peso = float(input("Peso:"))
+
+        except ValueError:
+            print ("Valor invalido!")
+            continue
+        
+        #obter informacao se vai ser pago no cartao tabajara.
+        tipo_pagamento = input("Pagar com cartão Tabajara?:")
+
+        #verifica o peso e ver qual tipo de valor a ser calculado.
+        if peso <= 5 and peso > 0:
+            #calculo com valor ate de 5kg.
+            total_preco = (peso * dados_produto[count][1])
+
+        elif peso > 5:
+            #calculo com valor acima de 5kg.
+            total_preco = (peso * dados_produto[count][2]
+
+    #verificar se o cliente tem o cartao tabajara.
+    if tipo_pagamento.lower() == 'sim':
+        titulo_pagamento = "Cartao Tabajara"
+        total_preco = total_preco * 5 / 100
+
+    else:
+       titulo_pagamento = "Dinheiro"
+
+    print ("\t\t\tNota Fiscal\n\tTipo:%s\n\tQuantidade:%.3fkg\n\tPreço Total:R$%.2f\n\tTipo Pagamento:%s\n\tValor a pagar:R$%.2f\n" % (tipo_carne, peso, total_preco, titulo_pagamento, total_preco))
+
+
+fazer_pedido()
