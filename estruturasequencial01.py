d0a942e59ab20d7d57940eacc3ab9bdf0f2e4a9e
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2014-02-12 14:16:45 -0200

    Estrutura Sequencial 01
    Primeiro exercício de estrutura sequencial.

---
+++ estruturasequencial01.py
@@ -0,0 +1,10 @@
+#! usr/bin/env python3
+
+############################################################
+#Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+############################################################
+
+def alo_mundo():
+
+    #imprimir Alô mundo.
+    print ("Alô mundo!")
