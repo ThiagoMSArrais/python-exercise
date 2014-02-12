b0c9a9fd9e97c5f6a8fde97f139fbd30e7f3b567
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2014-02-12 15:56:24 -0200

    Estrutura de Repetição 03
    Inserindo o enunciado do exercício Estrutura de Repetição 03

---
+++ estruturarepeticao03.py
@@ -0,0 +1,13 @@
+#! usr/bin/env python3
+
+#############################################################
+#Faça um programa que leia e valide as seguintes informações:
+#Nome: maior que 3 caracteres;
+#Idade: entre 0 e 150;
+#Salário: maior que zero;
+#Sexo: 'f' ou 'm';
+#Estado Civil: 's', 'c', 'v', 'd';
+##############################################################
+
+
+def validar_dados():
