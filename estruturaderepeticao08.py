efc2d8c759024ef38e768460994e96cc1e694d9a
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2014-02-13 19:01:30 -0200

    Estrutura de Repetição 08
    Inserindo o enunciado - primeira parte.

---
+++ estruturaderepeticao08.py
@@ -0,0 +1,16 @@
+#! usr/bin/env python3
+
+############################################################################
+#Faça um programa que leia 5 números e informe a soma e a média dos números.
+############################################################################
+
+
+#lista de números inteiros
+lista_numeros = []
+
+#iterar cinco vezes.
+for x in range(5):
+    #armazenar o valor na lista_numeros.
+    lista_numeros.append(int(input("Numero:")))
+    
+    #
