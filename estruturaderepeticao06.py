71f980d622431997a59a27fc2b86085fd154a526
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2014-02-13 18:27:13 -0200

    Estrutura de Repetição 06
    Inserindo a resposta do exercício de Estrutura de Repetição 06.

---
+++ estruturaderepeticao06.py
@@ -0,0 +1,17 @@
+#! usr/bin/env python3
+
+################################################################################
+#Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro.
+#Depois modifique o programa para que ele mostre os números um ao lado do outro.
+################################################################################
+
+
+def imprimir_numeros():
+
+    #fazer o looping.
+    for x in range(2):
+        #criar um looping mais interno.
+        for y in range(20):
+            #imprimir o valor em horizontal e depois em vertical,
+            #utilizando uma condição, para poder mudar a posição.
+            print (y + 1 + "\n" if x == 0 else y + 1 + " ")
