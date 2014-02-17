a934df21a241e56190feea28f6f33ebfe48e5b34
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2014-02-17 17:33:49 -0300

    Exercício Funções 03

---
+++ exerciciofuncao03.py
@@ -0,0 +1,13 @@
+#! usr/bin/env python3
+
+################################################################################################################
+#Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+################################################################################################################
+
+def soma(primeiro, segundo, terceiro):
+    #efetuar a soma dos três argumentos.
+    calc_soma = primeiro + segundo + terceiro
+    #retornar o valor já calculado.
+    return calc_soma
+
+soma(10,15,2)
